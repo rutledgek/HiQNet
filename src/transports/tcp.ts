17e51808bea0cf98cbd03c989bc9209d58c65237
rutledgek <kevin.m.rutledge@gmail.com>
2023-03-05 22:24:37 -0500

    Update tcp.ts
    Create a connect function that is not a

--- src/transports/tcp.ts
+++ src/transports/tcp.ts
@@ -8,16 +8,23 @@ class HiQnetTransportTcpUdp {
     private HIQNET_PORT = 3804;
     private tcpSocket!: Socket | null;
     private tcpHost: string;
-    private udpSocket!: udp.Socket | null;
-    private udpPort: number;
+    // private udpSocket!: udp.Socket | null;
+    // private udpPort: number;
     
 
-    constructor(udpPort: number, tcpHost: string) {
-       this.udpPort = udpPort;
+    constructor( tcpHost: string) {
+    //    this.udpPort = udpPort;
        this.tcpHost = tcpHost;
+       this.tcpSocket = new Socket();
+       this.tcpSocket.connect({ host: this.tcpHost, port:this.HIQNET_PORT })
+       this.tcpSocket.on('connect', () => {
+           console.log('Socket connected to server!');
+           // Do something once the socket is connected
+          
+         });
     }
 
-    async connect ()
+    connect ()
     {
         /*
         // Create a UDP Socket and bind it to the specified port
@@ -27,6 +34,16 @@ class HiQnetTransportTcpUdp {
         // Craete a TCP Socet and connect it to the specified hots and port
         this.tcpSocket = new Socket();
         this.tcpSocket.connect({ host: this.tcpHost, port:this.HIQNET_PORT })
+        this.tcpSocket.on('connect', () => {
+            console.log('Socket connected to server!');
+            // Do something once the socket is connected
+           
+          });
+    }
+
+    disconnect() 
+    {
+       this.tcpSocket?.end('Closing the connection');
     }
 }
 
