f86744b53789969e040c72e40c8d4a74bcc86c08
rutledgek <kevin.m.rutledge@gmail.com>
2023-03-05 22:01:30 -0500

    Remove upd socket info
    Remove udp socket creation until further information is gathered about the expected port on the machines.

--- src/transports/tcp.ts
+++ src/transports/tcp.ts
@@ -1,32 +1,32 @@
 import { Socket } from 'net';
-import udp from 'node:dgram';
-import { createSocket } from 'dgram';
+// import udp from 'node:dgram';
 
 
 
 class HiQnetTransportTcpUdp {
-
-    private udpSocket: udp.Socket | null;
-    private tcpSocket: Socket | null;
-    private udpPort: number;
+    
+    private HIQNET_PORT = 3804;
+    private tcpSocket!: Socket | null;
     private tcpHost: string;
-    private tcpPort: number; 
+    private udpSocket!: udp.Socket | null;
+    private udpPort: number;
+    
 
-    constructor(udpPort: number, tcpHost: string, tcpPort: number) {
+    constructor(udpPort: number, tcpHost: string) {
        this.udpPort = udpPort;
        this.tcpHost = tcpHost;
-       this.tcpPort = tcpPort;
     }
 
     async connect ()
     {
+        /*
         // Create a UDP Socket and bind it to the specified port
         this.udpSocket = createSocket({type: 'udp4', reuseAddr: true})
         this.udpSocket.bind(this.udpPort);
-
+        */
         // Craete a TCP Socet and connect it to the specified hots and port
         this.tcpSocket = new Socket();
-        this.tcpSocket.connect({ host: this.tcpHost, port:this.tcpPort })
+        this.tcpSocket.connect({ host: this.tcpHost, port:this.HIQNET_PORT })
     }
 }
 
