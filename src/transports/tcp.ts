006b34a0aa030240c744650ee79c157de9c3c410
rutledgek <kevin.m.rutledge@gmail.com>
2023-03-05 21:52:32 -0500

    Update the class constructor to save the values and add a connect function
    Add a connect function and update the constructor to save the ports passed to it in the class.

--- src/transports/tcp.ts
+++ src/transports/tcp.ts
@@ -6,23 +6,29 @@ import { createSocket } from 'dgram';
 
 class TcpTransport {
 
-    private udpSocket: DgramSocket | null;
+    private udpSocket: udp.Socket | null;
     private tcpSocket: Socket | null;
+    private udpPort: number;
+    private tcpHost: string;
+    private tcpPort: number; 
 
     constructor(udpPort: number, tcpHost: string, tcpPort: number) {
+       this.udpPort = udpPort;
+       this.tcpHost = tcpHost;
+       this.tcpPort = tcpPort;
+    }
+
+    async connect ()
+    {
         // Create a UDP Socket and bind it to the specified port
         this.udpSocket = createSocket({type: 'udp4', reuseAddr: true})
-        this.udpSocket.bind(udpPort);
+        this.udpSocket.bind(this.udpPort);
 
         // Craete a TCP Socet and connect it to the specified hots and port
         this.tcpSocket = new Socket();
-        this.tcpSocket.connect({ host: tcpHost, port:tcpPort })
-    
+        this.tcpSocket.connect({ host: this.tcpHost, port:this.tcpPort })
     }
-
-
 }
-
 
 
 
