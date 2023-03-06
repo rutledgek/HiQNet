799c2107d88f912cef5caffee7ce74a77ca6a2ed
rutledgek <kevin.m.rutledge@gmail.com>
2023-03-05 22:41:53 -0500

    Update the tcp transport
    Add handling of data and errors received on the tcp socket in the constructor.

--- src/transports/tcp.ts
+++ src/transports/tcp.ts
@@ -6,7 +6,7 @@ import { Socket } from 'net';
 class HiQnetTransportTcpUdp {
     
     private HIQNET_PORT = 3804;
-    private tcpSocket!: Socket | null;
+    private tcpSocket: Socket | null;
     private tcpHost: string;
     // private udpSocket!: udp.Socket | null;
     // private udpPort: number;
@@ -16,12 +16,20 @@ class HiQnetTransportTcpUdp {
     //    this.udpPort = udpPort;
        this.tcpHost = tcpHost;
        this.tcpSocket = new Socket();
-       this.tcpSocket.connect({ host: this.tcpHost, port:this.HIQNET_PORT })
        this.tcpSocket.on('connect', () => {
            console.log('Socket connected to server!');
            // Do something once the socket is connected
           
          });
+
+       this.tcpSocket.on('error', (error) => {
+            console.error(`TCP socket error: ${error.message}`);
+          });
+
+        this.tcpSocket.on('data', (data) => {
+            console.log(`TCP Socket Data: ${data}`)
+
+        });
     }
 
     connect ()
@@ -32,13 +40,7 @@ class HiQnetTransportTcpUdp {
         this.udpSocket.bind(this.udpPort);
         */
         // Craete a TCP Socet and connect it to the specified hots and port
-        this.tcpSocket = new Socket();
-        this.tcpSocket.connect({ host: this.tcpHost, port:this.HIQNET_PORT })
-        this.tcpSocket.on('connect', () => {
-            console.log('Socket connected to server!');
-            // Do something once the socket is connected
-           
-          });
+        this.tcpSocket?.connect({ host: this.tcpHost, port:this.HIQNET_PORT })
     }
 
     disconnect() 
